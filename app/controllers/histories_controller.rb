142680ed9739c037dd633a72d093391d001697f2
Mike <mike@otep.ch>
2017-11-19 15:45:50 +0100

    layouting and other view improvements

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,5 @@
 class HistoriesController < ApplicationController
   def show
-    @trackings = Tracking.where(label: @label=params[:id])
+    @trackings = Tracking.where(label: @label=params[:id]).order("created_at desc")
   end
 end
