6306beaa2ed7a06f8853d5bea7a23f5a6500ae44
Mike <mike@otep.ch>
2017-11-21 15:13:43 +0100

    prefer older images and images from different locations when displaying other occurances in current view #38

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,7 +11,19 @@ class LocationsController < ApplicationController
     @loc_name = params[:id]
     if @location = Location.where(name: @loc_name).first
       @tracking = @location.trackings.last
-      @history  = Tracking.where(label: @tracking.label).where.not(id: @tracking.id).includes(:tracked_image, :location).order("created_at desc").first(9)
+      base_scope = Tracking.where(label: @tracking.label).
+          where.not(id: @tracking.id).
+          includes(:tracked_image, :location).
+          order("trackings.created_at asc")
+      num_images = 6
+      @history  = base_scope.
+          joins(:tracked_image).
+          where.not(tracked_images: {location_id: @tracking.location.id}).
+          first(num_images)
+      count = @history.size
+      if count < num_images
+        @history += base_scope.first(num_images - count)
+      end
     end
   end
 
